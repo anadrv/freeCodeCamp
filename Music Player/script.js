14203c10c51a7399ac6cc50f0d457229a490accc
Ana Luisa <anadrvv@gmail.com>
2025-04-11 17:51:13 -0300

    att

--- Music Player/script.js
+++ Music Player/script.js
@@ -99,6 +99,7 @@ const playSong = (id) => {
   playButton.classList.add("playing");
 
   highlightCurrentSong();
+  setPlayerDisplay();
   audio.play();
 };
 
@@ -135,7 +136,9 @@ const setPlayerDisplay = () => {
   const songArtist = document.getElementById("player-song-artist");
   const currentTitle = userData?.currentSong?.title;
   const currentArtist = userData?.currentSong?.artist;
-  
+
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
 };
 
 const highlightCurrentSong = () => {
@@ -171,6 +174,10 @@ const renderSongs = (array) => {
     .join("");
 
   playlistSongs.innerHTML = songsHTML;
+};
+
+const setPlayButtonAccessibleText = () => {
+  
 };
 
 const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);

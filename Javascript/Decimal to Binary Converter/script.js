4e7700a7a71e967a7e853cd76c0fef780c29cf67
Ana Luisa <anadrvv@gmail.com>
2025-06-28 21:52:37 -0300

    recursao

--- Javascript/Decimal to Binary Converter/script.js
+++ Javascript/Decimal to Binary Converter/script.js
@@ -2,27 +2,14 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const countdown = (number) => {
-  console.log(number);
-
-  if (number === 0) {
-    return;
-  }
-};
-
 const decimalToBinary = (input) => {
-  let binary = "";
-
   if (input === 0) {
-    binary = "0";
+    return "0";
+  } 
+  
+  else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
-
-  while (input > 0) {
-    binary = (input % 2) + binary;
-    input = Math.floor(input / 2);
-  }
-
-  result.innerText = binary;
 };
 
 const checkUserInput = () => {
@@ -35,7 +22,7 @@ const checkUserInput = () => {
     return;
   }
 
-  decimalToBinary(parseInt(numberInput.value));
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
   numberInput.value = "";
 };
 

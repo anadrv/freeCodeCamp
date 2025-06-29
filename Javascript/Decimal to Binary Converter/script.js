e29a3028fd1e616978c642074e578a7f13879bd1
Ana Luisa <anadrvv@gmail.com>
2025-06-28 22:10:17 -0300

    parseInt

--- Javascript/Decimal to Binary Converter/script.js
+++ Javascript/Decimal to Binary Converter/script.js
@@ -10,19 +10,26 @@ const decimalToBinary = (input) => {
   }
 };
 
-
+const showAnimation = () => {
+  console.log("free");
+  console.log("Code");
+  console.log("Camp");
+};
 
 const checkUserInput = () => {
-  if (
-    !numberInput.value ||
-    isNaN(parseInt(numberInput.value)) ||
-    parseInt(numberInput.value) < 0
-  ) {
+  const inputInt = parseInt(numberInput.value);
+
+  if (!numberInput.value || isNaN(inputInt) || inputInt < 0) {
     alert("Please provide a decimal number greater than or equal to 0");
     return;
   }
 
-  result.textContent = decimalToBinary(parseInt(numberInput.value));
+  if (inputInt === 5) {
+    showAnimation();
+    return;
+  }
+
+  result.textContent = decimalToBinary(inputInt);
   numberInput.value = "";
 };
 

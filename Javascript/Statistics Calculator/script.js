70263af69649c8f1f0d40a6718a515c554bfc2c5
Ana Luisa <anadrvv@gmail.com>
2025-07-27 20:24:52 -0300

    getMode

--- Javascript/Statistics Calculator/script.js
+++ Javascript/Statistics Calculator/script.js
@@ -9,15 +9,24 @@ const getMedian = (array) => {
   return median;
 }
 
+const getMode = (array) => {
+  const counts = {};
+  array.forEach(el => {
+
+  })
+  return array;
+}
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-
+  
   const mean = getMean(numbers);
   const median = getMedian(numbers);
+  console.log(getMode(numbers));
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
-
 }
+

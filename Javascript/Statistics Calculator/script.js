c6556cba941c6b2a888e6da8d9a45d5cdc0b80c8
Ana Luisa <anadrvv@gmail.com>
2025-07-30 21:08:21 -0300

    feat(js): getRange function

--- Javascript/Statistics Calculator/script.js
+++ Javascript/Statistics Calculator/script.js
@@ -26,6 +26,9 @@ const getMode = (array) => {
   return mode.join(", ");
 }
 
+const getRange = (array) => {
+  return Math.max(...array) - Math.min(...array);
+}
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
@@ -35,10 +38,12 @@ const calculate = () => {
   const mean = getMean(numbers);
   const median = getMedian(numbers);
   const mode = getMode(numbers);
+  const range = getRange(numbers);
 
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
   document.querySelector("#mode").textContent = mode;
+  document.querySelector("#range").textContent = range;
 
 }

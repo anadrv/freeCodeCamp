6cab067c28a08105b219f535e5fcac2abe585230
Ana Luisa <anadrvv@gmail.com>
2025-08-27 21:00:06 -0300

    update

--- Javascript/Spreadsheet/script.js
+++ Javascript/Spreadsheet/script.js
@@ -26,10 +26,11 @@ const evalFormula = (x, cells) => {
   const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
   const elemValue = num => {
     const inner = character => {
-
+      return idToText(character + num);
     }
     return inner;
   }
+
 }
 
 window.onload = () => {

83df132fcc029ce83eb804a29363ef99baf11bfc
Ana Luisa <anadrvv@gmail.com>
2026-07-14 14:24:24 -0300

    Trivia Bot

---
+++ Javascript/TriviaBot.js
@@ -0,0 +1,11 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Jungsu";
+const botLocation = "Korea";
+let favoriteLanguage = "Javascript";
+let codingFact = `ICU - ${favoriteLanguage}`;
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+console.log(codingFact);

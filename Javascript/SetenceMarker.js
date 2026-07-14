375b676bc6541fe5f391103fa61650aac168845d
Ana Luisa <anadrvv@gmail.com>
2026-07-14 14:48:00 -0300

    setence marker

---
+++ Javascript/SetenceMarker.js
@@ -0,0 +1,21 @@
+let adjective = "weird";
+let noun = "girl";
+let verb = "running";
+let place = "tree";
+let adjective2 = "happy";
+let noun2 = "flowers"; 
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log('First story: ' + firstStory);
+
+adjective = "";
+noun = "";
+verb = "";
+place = "";
+adjective2 = "";
+noun2 = ""; 
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log('Second story: ' + secondStory);

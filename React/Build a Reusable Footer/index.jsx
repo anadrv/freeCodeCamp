c20730daca0e3dbb26999f23815f1f27ca6e63fe
Ana Luisa <anadrvv@gmail.com>
2026-07-31 20:49:17 -0300

    footer

---
+++ React/Build a Reusable Footer/index.jsx
@@ -0,0 +1,34 @@
+export const Footer = () => {
+  return(
+    <footer>
+      <ul>
+      <li className="nav-item">
+          <a href='#'>Keeho</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">Theo</a>
+        </li>
+      </ul>
+
+      <ul>
+      <li className="nav-item">
+          <a href='#'>Jiung</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">Intak</a>
+        </li>
+      </ul>
+
+      <ul>
+      <li className="nav-item">
+          <a href='#'>Soul</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">Jongseob</a>
+        </li>
+      </ul>
+
+      <p> © P1HARMONY </p>
+    </footer>
+  )
+}
